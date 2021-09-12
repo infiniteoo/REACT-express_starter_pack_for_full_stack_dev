858c8813992fec6bc38917eca050fe924b9c0e32
Troy Dorman <troydorman@gmail.com>
2021-09-11 20:33:43 -0600

    created state for customers. useeffect hook fetches customers from backend. successfully teted

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,6 +1,18 @@
 import "./customers.css";
+import { useState, useEffect } from "react";
 
 function Customers() {
+  useEffect(() => {
+    fetch("/api/customers")
+      .then((res) => res.json())
+      .then(
+        (customers) => setCustomers(customers),
+        () => console.log("customers fetched..", customers)
+      );
+  }, []);
+
+  const [customers, setCustomers] = useState([]);
+
   return (
     <div>
       <h2>Customers</h2>
